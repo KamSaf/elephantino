a281ad9bc3f96c514a539d818cee1f36b45d438c
KamSaf <bopaf42@gmail.com>
2024-09-13 15:16:17 +0200

    Create init script

---
+++ scripts/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Setting up Elephantino skeleton app...\r\n"
+cp ./vendor/kamsaf/elephantino/scripts/base/index.php .
+cp ./vendor/kamsaf/elephantino/scripts/base/main.php .
+
+echo "Setup completed!\r\n "
